1c5c43252a79235d0850f2e571c6d32b66be2f6f
Juraci Vieira <juraci.vieira@gmail.com>
2015-12-01 14:43:55 -0200

    sharksWithLasers clousure exercise

---
+++ spec/sharksWithLasers/sharksWithLasersSpec.js
@@ -0,0 +1,26 @@
+describe('sharksWithLasers', function (){
+    var makeTargetAssigner = require('../../lib/sharksWithLasers/sharksWithLasers.js');
+    var alert;
+
+    var listOfSharks = ["Sea Pain", "Great Wheezy",
+        "DJ Chewie", "Lil' Bitey",
+        "Finmaster Flex", "Swim Khalifa",
+        "Ice Teeth", "The Notorious J.A.W."];
+
+    var listOfTargets = ["icicle bat", "snow yeti",
+        "killer penguin", "frost tiger",
+        "polar bear", "iceberg",
+        "blue witch", "wooly mammoth"];
+
+    beforeEach(function (){
+        alert = jasmine.createSpy('alert');
+    });
+
+    it('should alert a message when the indexes matches', function (){
+        var getTargetFor = makeTargetAssigner(listOfSharks, listOfTargets, alert);
+        getTargetFor("Ice Teeth");
+        expect(alert).toHaveBeenCalledWith("What up, Ice Teeth!\n" +
+                                           "There've been blue witch sightings in our hood!\n" +
+                                           "Time for a swim-by lasering, homie!");
+    });
+});
