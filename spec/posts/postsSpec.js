bba2cee8820eb1e4b59a3c6b722a138fb96e7fdb
Juraci Vieira <juraci.vieira@gmail.com>
2015-12-14 14:46:01 -0200

    Refactoring the posts to use the constructor pattern for create Objects

--- spec/posts/postsSpec.js
+++ spec/posts/postsSpec.js
@@ -1,18 +1,11 @@
 describe('Posts', function (){
-    var genericPost = require('../../lib/posts/posts.js');
+    var Fencepost = require('../../lib/posts/posts.js');
     var post1;
     var post2;
 
     it('should connect 2 posts', function (){
-        post1 =  Object.create(genericPost);
-        post1.x = -2;
-        post1.y = 4;
-        post1.postNum = 1;
-
-        post2 = Object.create(genericPost);
-        post2.x = 5;
-        post2.y = 1;
-        post2.postNum = 2;
+        post1 =  new Fencepost(-2, 4, 1);
+        post2 = new Fencepost(5, 1, 2);
 
         post1.sendRopeTo(post2);
         post2.sendRopeTo(post1);
